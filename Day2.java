274e705adca7140316f88d25301cdafef1d9067e
rindi223044 <rindisafitri926@gmail.com>
2025-09-09 21:01:57 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Nama");
+        System.out.println("Saya Riri");
+        
+        System.out.print("Hello ");
+        System.out.print("World \n");
+       
+        
+        String Nama = "Rindi";
+        int umur = 20;
+        System.out.printf("Haloo, perkenalkan nama saya, %s dan umur saya %d  tahun\n",Nama,umur );
+       
+    }
+    
+}
