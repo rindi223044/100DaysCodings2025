6ff1147e73edbaf96e32de28ca9ee2fbdf1c6f38
rindi223044 <rindisafitri926@gmail.com>
2025-09-11 18:56:06 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,8 @@
+public class EscapeSequenceDemo {
+    public static void main(String[] args) {
+        System.out.println("Nama:\tJohn Doe");
+        System.out.println("Alamat:\tJl. Merpati No. 10\n\tKota Bandung\n\tJawa Barat");
+        System.out.println("Catatan:\n\"Jangan lupa membawa KTP saat registrasi.\"");
+        System.out.println("Folder path: C:\\Users\\JohnDoe\\Documents");
+    }
+}
