9e005efe4feb19ed7291c68a6e334358134f22a6
rindi223044 <rindisafitri926@gmail.com>
2025-09-13 02:31:15 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,14 @@
+public class OperasiPecahan {
+    public static void main(String[] args) {
+        float a = 5.5f;
+        float b = 2.0f;
+        float hasilFloat = a / b;
+
+        double x = 10.75;
+        double y = 4.25;
+        double hasilDouble = x * y;
+
+        System.out.println("Hasil pembagian float  : " + hasilFloat);
+        System.out.println("Hasil perkalian double : " + hasilDouble);
+    }
+}
