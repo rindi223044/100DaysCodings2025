8f4bd5b4185900018dbc8020e1b65c5b9837d35f
rindi223044 <rindisafitri926@gmail.com>
2025-09-18 19:52:15 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+        // Deklarasi
+        int umur;
+        String nama;
+
+        // Inisialisasi
+        umur = 20;
+        nama = "Andi";
+
+        // Tampilkan nilai awal
+        System.out.println("Nama: " + nama + ", Umur: " + umur);
+
+        // Update nilai
