35f1411f5797908cf6dab516d264ee19c87c12a8
rindi223044 <rindisafitri926@gmail.com>
2025-09-22 22:49:32 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class Aritmatika {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+
+        // Penjumlahan
+        int hasilTambah = angka1 + angka2;
+
+        // Pengurangan
+        int hasilKurang = angka1 - angka2;
+
+        System.out.println("Hasil Penjumlahan: " + hasilTambah);
+        System.out.println("Hasil Pengurangan: " + hasilKurang);
+    }
+}
