39c374816972facbdb43883e78b62fdfa113022b
rindi223044 <rindisafitri926@gmail.com>
2025-09-23 21:11:20 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,17 @@
+public class Aritmatika {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int angka1 = 20;
+        int angka2 = 4;
+
+        // Operator perkalian
+        int hasilPerkalian = angka1 * angka2;
+
+        // Operator pembagian
+        int hasilPembagian = angka1 / angka2;
+
+        // Menampilkan hasil
+        System.out.println("Hasil Perkalian: " + hasilPerkalian); // 20 * 4 = 80
+        System.out.println("Hasil Pembagian: " + hasilPembagian); // 20 / 4 = 5
+    }
+}
