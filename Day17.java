fd4ffbb4331e8fc1fb0c0bc2112ca1c8ad93078b
rindi223044 <rindisafitri926@gmail.com>
2025-09-24 20:48:35 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,12 @@
+public class ModulusExample {
+    public static void main(String[] args) {
+        int angka1 = 17;
+        int angka2 = 5;
+
+        int sisaBagi = angka1 % angka2; // operasi modulus
+
+        System.out.println("Angka 1: " + angka1);
+        System.out.println("Angka 2: " + angka2);
+        System.out.println("Sisa bagi dari " + angka1 + " % " + angka2 + " = " + sisaBagi);
+    }
+}
