948765c16ea841b9fa956175afeab4582471a329
rindi223044 <rindisafitri926@gmail.com>
2025-10-02 20:43:15 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int angka = 15;
+
+        System.out.println("Nilai awal: " + angka);
+
+        angka++; // increment: menambah nilai 1
+        System.out.println("Setelah increment (angka++): " + angka);
+
+        angka--; // decrement: mengurangi nilai 1
+        System.out.println("Setelah decrement (angka--): " + angka);
+    }
+}
