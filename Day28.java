1dcb94e8df98c596473f3953ec31a277db43f09f
rindi223044 <rindisafitri926@gmail.com>
2025-10-05 21:55:13 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,12 @@
+public class Day028 {
+    public static void main(String[] args) {
+        int angka1 = 7;
+        int angka2 = 7;
+
+        boolean hasil1 = (angka1 == angka2);
+        boolean hasil2 = (angka1 != angka2);
+
+        System.out.println("Apakah angka1 == angka2? " + hasil1);
+        System.out.println("Apakah angka1 != angka2? " + hasil2);
+    }
+}
