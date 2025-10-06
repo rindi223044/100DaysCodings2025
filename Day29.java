7070170589c33ec2eab1b4a3d9fd230e8a97adb4
rindi223044 <rindisafitri926@gmail.com>
2025-10-06 23:18:03 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,16 @@
+public class Perbandingan {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 20;
+
+        // Operator <
+        if (angka1 < angka2) {
+            System.out.println("angka1 lebih kecil dari angka2");
+        }
+
+        // Operator >
+        if (angka2 > angka1) {
+            System.out.println("angka2 lebih besar dari angka1");
+        }
+    }
+}
