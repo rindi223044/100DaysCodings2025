19c9d29af534bca0a6375753cbf093f90f7809b8
rindi223044 <rindisafitri926@gmail.com>
2025-10-07 23:51:44 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,16 @@
+public class PerbandinganAngka {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Operator lebih besar
+        if (a > b) {
+            System.out.println(a + " lebih besar dari " + b);
+        }
+
+        // Operator lebih kecil
+        if (b < a) {
+            System.out.println(b + " lebih kecil dari " + a);
+        }
+    }
+}
