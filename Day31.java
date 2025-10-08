d6b7ff582b3cdeecc7f59f6dfb1fe85d9a3f45a1
rindi223044 <rindisafitri926@gmail.com>
2025-10-08 21:16:39 +0800

    Create Day31.java

---
+++ Day31.java
@@ -0,0 +1,12 @@
+public class LogikaAndDemo {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        // Menggunakan operator logika AND
+        boolean hasil = a && b;
+
+        // Cetak hasil
+        System.out.println("a AND b = " + hasil);
+    }
+}
