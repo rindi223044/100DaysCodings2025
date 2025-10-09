2791cbcca224f43c8519cb400882f31d178dcca4
rindi223044 <rindisafitri926@gmail.com>
2025-10-09 23:57:58 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,13 @@
+public class LogikaOR {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        // Menggunakan operator logika OR (||)
+        if (x > 3 || y < 5) {
+            System.out.println("Salah satu kondisi benar.");
+        } else {
+            System.out.println("Kedua kondisi salah.");
+        }
+    }
+}
