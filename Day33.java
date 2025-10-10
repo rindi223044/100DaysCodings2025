6eff2c008dad45acb9113540626adea8401f7b12
rindi223044 <rindisafitri926@gmail.com>
2025-10-10 20:48:50 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,11 @@
+public class OperatorNot {
+    public static void main(String[] args) {
+        boolean status = true;
+
+        // Menggunakan operator NOT (!)
+        boolean hasil = !status;
+
+        System.out.println("Nilai awal status: " + status);
+        System.out.println("Setelah menggunakan operator NOT: " + hasil);
+    }
+}
