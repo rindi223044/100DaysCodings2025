8c61de8992c8323cc93280d13b02d8b3eeecc245
rindi223044 <rindisafitri926@gmail.com>
2025-10-12 22:57:44 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,12 @@
+public class PercabanganIf {
+    public static void main(String[] args) {
+        int nilai = 75; // nilai siswa
+
+        // Percabangan if
+        if (nilai >= 70) {
+            System.out.println("Selamat, kamu lulus!");
+        }
+
+        System.out.println("Program selesai.");
+    }
+}
